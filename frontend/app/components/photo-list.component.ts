7bc61a69d52e14bc539091d43cba69b5d20b37b9
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2016-06-29 12:13:19 +0200

    Ignore frontend build files.

---
+++ frontend/app/components/photo-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'photo-list',
+  templateUrl: 'app/components/photo-list.component.html'
+})
+export class PhotoList {
+
+}
---
+++ frontend/app/components/register.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component ({
+  selector: 'register',
+  templateUrl: 'app/components/register.component.html'
+})
+export class Register {
+
+}
