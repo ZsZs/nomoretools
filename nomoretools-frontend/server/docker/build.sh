debe3862e8d7c2b07593f413e45467c678d35af3
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2016-08-21 11:51:32 +0200

    Correct docker image name.

--- nomoretools-frontend/server/docker/build.sh
+++ nomoretools-frontend/server/docker/build.sh
@@ -1,5 +1,5 @@
 rm -fr build
 mkdir build
 cp ../target/nomoretools-frontend-server-1.0.0-SNAPSHOT.jar build
-docker build -t zsuffazs/nomoretools-frontend --rm=true .
-docker run -d -p 8080:8080 --name nomoretools-frontend zsuffazs/nomoretools-frontend
+docker build -t zsuffazs/nomoretools-frontend-server --rm=true .
+docker run -d -p 8080:8080 --name nomoretools-frontend-server zsuffazs/nomoretools-frontend-server
