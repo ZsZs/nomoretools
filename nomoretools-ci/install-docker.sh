d88519448e6f18ef72dda82cceb5d35a56b94f5a
ZsZs <zsolt.zsuffa@gmail.com>
2017-04-03 07:17:20 +0200

    Add CI_TOOLSET_DATA_HOME variable.

--- nomoretools-ci/install-docker.sh
+++ nomoretools-ci/install-docker.sh
@@ -3,8 +3,11 @@ sudo apt-get install apt-transport-https ca-certificates curl software-propertie
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install docker-ce -y
 sudo chown -R $(whoami) /usr/local/bin
 curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+export CI_TOOLSET_DATA_HOME=/data
+sudo chown 1000 /data/jenkins
