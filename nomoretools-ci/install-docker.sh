d881b475b1da64f9140116fd42db6adb3f4775db
ZsZs <zsolt.zsuffa@gmail.com>
2017-04-03 05:46:04 +0200

    compile script to install docker and docker-compose

--- nomoretools-ci/install-docker.sh
+++ nomoretools-ci/install-docker.sh
@@ -1,9 +1,10 @@
+sudo apt-get remove docker docker-engine
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+sudo chown -R $(whoami) /usr/local/bin
 curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-
 chmod +x /usr/local/bin/docker-compose
